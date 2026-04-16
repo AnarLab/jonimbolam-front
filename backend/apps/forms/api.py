6c4558837ede23de5f48c878bd27d87dadb62c54
STQT <productgsm@gmail.com>
2026-04-16 11:00:35 +0500

    fix

--- backend/apps/forms/api.py
+++ backend/apps/forms/api.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,6 +14,7 @@ class ContactFormSerializer(serializers.Serializer):
     message = serializers.CharField(required=False, allow_blank=True)
 
 
+@method_decorator(csrf_exempt, name="dispatch")
 class ContactFormView(APIView):
     def post(self, request):
         serializer = ContactFormSerializer(data=request.data)
